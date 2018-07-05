05ea4a8280208e0e0af802b801b4a59acf790505
sunqi <sunqi@viewhigh.com>
2018-07-05 15:29:18 +0800

    修改注释
    修改注释

--- java-class/src/main/java/com/qige/service/People.java
+++ java-class/src/main/java/com/qige/service/People.java
@@ -5,6 +5,7 @@ import com.qige.annotion.CollectTarget;
 
 @CollectTarget
 public class People {
+	// use the annotion @CollectMethod ,the spring context can collect the method name
 	@CollectMethod("collectGetPeopleName")
 	public String getPeopleName() {
 		return "sunqi";
